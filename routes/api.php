398cc8af0e4f91e2b4835cc1e7614b60450e6599
Feyfa <fisikamodern00@gmail.com>
2025-01-05 12:50:47 +0700

    testing route

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes Jidan Ganteng
+| API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/jidan/test', fn () => "Ini Jidan Test");
