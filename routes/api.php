34d2134867e8c5fca8bbbe5fde2a84c38a035f48
Feyfa <fisikamodern00@gmail.com>
2025-01-05 12:47:08 +0700

    testing api

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes Jidan
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
